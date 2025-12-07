2cbd3e0e5776263e138c986beb7cfa2fb3beceac
unknown <natalyfru@yandex.ru>
2025-12-07 20:01:06 +0300

    services

--- src/components/Services/Rides/Group/GroupPage.tsx
+++ src/components/Services/Rides/Group/GroupPage.tsx
@@ -1,16 +1,7 @@
-import {
-  AboutTeaser,
-  Article,
-  Container,
-  GallerySection,
-  Hero,
-  Highlights,
-  SafetyNotice,
-} from "@/ui";
+import { AboutTeaser, Container, GallerySection, Hero } from "@/ui";
 import { dataTextAboutTeaser, imagesGroup } from "./dataGroup";
 import dataPrice from "./dataPriceApi";
 import { ServicesList } from "../../ServicesList";
-import { ComponentsGrid } from "../../ComponentList";
 
 const GroupPage = () => {
   return (
@@ -27,22 +18,6 @@ const GroupPage = () => {
         <AboutTeaser {...dataTextAboutTeaser}></AboutTeaser>
         <GallerySection items={imagesGroup} columns={3} />
         <ServicesList items={dataPrice} columns={2} />
-        <Article title="Article" content={<div>Контент</div>} />
-
-        <Highlights items={[{ title: "Highlight", description: "string" }]} />
-        <ComponentsGrid components={[]} />
-        <SafetyNotice
-          title="SafetyNotice"
-          items={["jhgjhgjh", "jhgjhgjhg", "khgjhghjgjh"]}
-        />
-        <SafetyNotice
-          title="SafetyNotice"
-          items={["jhgjhgjh", "jhgjhgjhg", "khgjhghjgjh"]}
-        />
-        <SafetyNotice
-          title="SafetyNotice"
-          items={["jhgjhgjh", "jhgjhgjhg", "khgjhghjgjh"]}
-        />
       </Container>
     </div>
   );
